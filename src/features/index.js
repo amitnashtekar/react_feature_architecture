8ff508f19110c5a9c3aa051eb834c22348afdcb9
Amit Ashtekar <Amit.Ashtekar@amdocs.com>
2018-02-18 19:38:05 +1100

    redux setup and connect to cust_search component

---
+++ src/features/index.js
@@ -0,0 +1,1 @@
+export { default as CustomerSearch} from './customer-search/customer-search'
--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import configureStore from './app/store/store';
 import { Provider } from 'react-redux';
-import CustomerSearch from './features/customer-search/customer-search.js';
+import {CustomerSearch} from './features';
 
 const store = configureStore( window.REDUX_INITIAL_DATA );
 
